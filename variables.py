f2b6430067cc66fdd407fb2acb5f65402def2001
mohamed.aashik <aashikm015@gmail.com>
2023-03-05 15:46:56 +0530

    Variable file added

---
+++ variables.py
@@ -0,0 +1,18 @@
+#This is comment
+
+'''
+This is a 
+multiline comment
+'''
+
+print("Hello Wold. Welcome ...\n")
+a=5
+b=3
+print(a,"+",b,"= is ",a+b)
+print(a,"-",b,"= is ",a-b)
+print(a,"*",b,"= is ",a*b)
+print(a,"/",b,"= is ",a/b)
+print(a,"//",b,"= is ",a//b)
+print(a,"**",b,"= is ",a**b)
+
+
